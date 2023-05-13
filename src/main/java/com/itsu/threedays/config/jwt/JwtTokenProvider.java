de0dd56437ad7050631bfd85d3775bb168f3c3c5
jiixon <jr0107380@gmail.com>
2023-05-13 19:01:22 +0900

    REFACTOR: JWT TOKEN Configuration

---
+++ src/main/java/com/itsu/threedays/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,97 @@
+package com.itsu.threedays.config.jwt;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @PostConstruct
+    private void init() {
+        secret = Base64.getEncoder().encodeToString(secret.getBytes());
+    }
+
+    public String createToken(Authentication authentication){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 86400);
+//        Claims claims = Jwts.claims().setSubject(String.valueOf(authentication.getPrincipal()));
+//        claims.put("role",authentication.getAuthorities());
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth",authorities)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256,secret)
+                .compact();
+    }
+    //토큰넘버, 만료인증시간(24시간정도),,
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            //토큰의 만료 시간이 현재 시간 이전인지를 확인 -> 만료 시간이 현재 시간 이전이라면 유효하지 않은 토큰으로 판단
+            if (claims.getBody().getExpiration().before(new Date())) {
+                return false;
+            }
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    // 인증 성공시 SecurityContextHolder에 저장할 Authentication 객체 생성
+    public Authentication getAuthentication(String token) {
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        //클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get("auth").toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        //User 객체를 만들어서 Authentication 리턴
+        User principal = new User(claims.getSubject(), "",authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+    public String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        log.info("bearerToken: {}",bearerToken);
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) { //authorization이 Bearer 인지 확인
+
+            log.info("bearerToken.substring(7) :{}",bearerToken.substring(7));
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+}
