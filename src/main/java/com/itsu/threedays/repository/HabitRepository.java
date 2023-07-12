cd3df3d2d71beaab236f4b9a21f59eb76227bba3
jiixon <jr0107380@gmail.com>
2023-07-13 01:15:41 +0900

    feat: user profile

---
+++ src/main/java/com/itsu/threedays/repository/HabitRepository.java
@@ -0,0 +1,17 @@
+package com.itsu.threedays.repository;
+
+import com.itsu.threedays.entity.HabitEntity;
+import com.itsu.threedays.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HabitRepository extends JpaRepository<HabitEntity, Long> {
+    List<HabitEntity> findAllByUserId(UserEntity userId);
+
+    Optional<HabitEntity> findById(Long habitId);
+
+}
