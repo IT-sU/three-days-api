f5b9d3db36124b92d3f6973e8619f57c0e70a98d
jiixon <jr0107380@gmail.com>
2023-07-23 16:24:19 +0900

    refactor: 안쓰이는 import 삭제

--- src/main/java/com/itsu/threedays/controller/FollowController.java
+++ src/main/java/com/itsu/threedays/controller/FollowController.java
@@ -1,7 +1,5 @@
 package com.itsu.threedays.controller;
 
-import com.itsu.threedays.dto.FollowUserDto;
-import com.itsu.threedays.entity.UserEntity;
 import com.itsu.threedays.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FollowController {
     private final FollowService followService;
 
-    @PostMapping("{fromUserId}/follow/{toUserId}")//팔로우하기
+    @PostMapping("{fromUserId}/follow/{toUserId}")
+//팔로우하기
     ResponseEntity<?> Follow(@PathVariable Long fromUserId, @PathVariable Long toUserId) {
         followService.followUser(fromUserId, toUserId);
         return new ResponseEntity<>(HttpStatus.OK);
